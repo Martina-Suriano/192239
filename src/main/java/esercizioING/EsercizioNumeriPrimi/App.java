a3d6436d40f9468cd7902f0047e94f8119a6ec01
Martina Suriano <martinasuriano15@gmail.com>
2020-10-28 12:29:13 +0100

    Primo commit

---
+++ src/main/java/esercizioING/EsercizioNumeriPrimi/App.java
@@ -0,0 +1,14 @@
+package esercizioING.EsercizioNumeriPrimi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MyMath math= new MyMath();
+        System.out.println(math.primeNumbersGenerator(20));
+    }
+}
